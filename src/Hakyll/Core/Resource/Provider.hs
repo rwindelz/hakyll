03f3194772ec3b0f5db809201d133f387c6e3f89
Jasper Van der Jeugt <m@jaspervdj.be>
2012-08-15 17:12:40 +0900

    Wrap metadata cache functions before exposing

--- src/Hakyll/Core/Resource/Provider.hs
+++ src/Hakyll/Core/Resource/Provider.hs
@@ -18,6 +18,23 @@ module Hakyll.Core.Resource.Provider
 
 
 --------------------------------------------------------------------------------
+import           Hakyll.Core.Resource
 import           Hakyll.Core.Resource.Provider.Internal
-import           Hakyll.Core.Resource.Provider.MetadataCache
+import qualified Hakyll.Core.Resource.Provider.MetadataCache as Internal
 import           Hakyll.Core.Resource.Provider.Modified
+
+
+--------------------------------------------------------------------------------
+-- | Wrapper to ensure metadata cache is invalidated if necessary
+resourceMetadata :: ResourceProvider -> Resource -> IO Metadata
+resourceMetadata rp r = do
+    _ <- resourceModified rp r
+    Internal.resourceMetadata rp r
+
+
+--------------------------------------------------------------------------------
+-- | Wrapper to ensure metadata cache is invalidated if necessary
+resourceBody :: ResourceProvider -> Resource -> IO String
+resourceBody rp r = do
+    _ <- resourceModified rp r
+    Internal.resourceBody rp r
