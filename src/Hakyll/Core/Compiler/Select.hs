04c7f2c61aa0b426e0b98d87d1af2e7ce70b76d5
Jasper Van der Jeugt <m@jaspervdj.be>
2012-08-15 09:57:01 +0900

    Add selectItem & refactor a bit

--- src/Hakyll/Core/Compiler/Select.hs
+++ src/Hakyll/Core/Compiler/Select.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE UndecidableInstances   #-}
 module Hakyll.Core.Compiler.Select
     ( selectResource
+    , selectItem
     , selectItems
     ) where
 
@@ -23,28 +24,26 @@ import           Data.Typeable                 (Typeable)
 import           Hakyll.Core.Compiler.Internal
 import           Hakyll.Core.Compiler.Store
 import           Hakyll.Core.Item
+import           Hakyll.Core.Populate
 import           Hakyll.Core.Resource
 
 
 --------------------------------------------------------------------------------
 selectResource :: (Binary a, Typeable a) => Resource -> Compiler i b a
-selectResource rs = fromDependencies ids >>> fromJob job
+selectResource rs = selectSingle
+    "Hakyll.Core.Compiler.Selector.selectResource" (unResource rs) $ \pop ->
+        [id' | (id', (SomeItem item, _)) <- pop , itemResource item == Just rs]
+
+
+--------------------------------------------------------------------------------
+selectItem :: (Binary a, Eq i, Show i, Typeable a)
+           => (Item a -> i) -> Compiler i b a
+selectItem selector = selectSingle
+    "Hakyll.Core.Compiler.Selector.selectItem" (show selector') $ \pop ->
+        [id' | (id', (_, userdata)) <- pop , userdata == selector']
   where
-    ids pop =
-        [ itemIdentifier item
-        | (_, (SomeItem item, _)) <- pop
-        , itemResource item == Just rs
-        ]
-
-    job = \_ -> CompilerM $ do
-        ids' <- ids . compilerPopulation <$> ask
-        case ids' of
-            [id']   -> unCompilerM $ fromStore id'
-            []      -> throwError' $ "No item matched " ++ unResource rs
-            (_ : _) -> throwError' $ "Multiple items matched " ++ unResource rs
-
-    throwError' e = throwError $
-        "Hakyll.Core.Compiler.Selector.selectResource: " ++ e
+    selector' = selector $ error
+        "Hakyll.Core.Compiler.Selector.selectItem: Internal error"
 
 
 --------------------------------------------------------------------------------
@@ -55,6 +54,25 @@ selectItems selector = fromDependencies ids >>> fromJob job
     job   = \_ -> CompilerM $ do
         ids' <- ids . compilerPopulation <$> ask
         mapM (unCompilerM . fromStore) ids'
+
+
+--------------------------------------------------------------------------------
+-- | Auxiliary: select a single item
+selectSingle :: (Binary a, Typeable a)
+             => String                              -- ^ Higher function name
+             -> String                              -- ^ Readable item name
+             -> (Population i -> [ItemIdentifier])  -- ^ Select ID's
+             -> Compiler i b a                      -- ^ Resulting compiler
+selectSingle fname iname ids = fromDependencies ids >>> fromJob job
+  where
+    job = \_ -> CompilerM $ do
+        ids' <- ids . compilerPopulation <$> ask
+        case ids' of
+            [id']   -> unCompilerM $ fromStore id'
+            []      -> throwError' $ "No item matched: " ++ iname
+            (_ : _) -> throwError' $ "Multiple items matched: " ++ iname
+
+    throwError' e = throwError $ fname ++ ": " ++ e
 
 
 --------------------------------------------------------------------------------
